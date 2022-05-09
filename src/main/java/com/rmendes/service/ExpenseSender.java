fc18ac9235d49a1397dcb3fdab8c2ab5798adb0a
Raffael Mendes <raffael.pikachu@gmail.com>
2022-05-09 15:43:29 -0300

    Adjusting for publishing

--- src/main/java/com/rmendes/service/ExpenseSender.java
+++ src/main/java/com/rmendes/service/ExpenseSender.java
@@ -1,27 +1,23 @@
 package com.rmendes.service;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
+import java.nio.charset.StandardCharsets;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 import com.rmendes.model.Expense;
 
+import org.apache.kafka.common.header.internals.RecordHeaders;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.eclipse.microprofile.reactive.messaging.Message;
+import org.jboss.logging.Logger;
 
-import io.smallrye.mutiny.Uni;
 import io.smallrye.reactive.messaging.MutinyEmitter;
+import io.smallrye.reactive.messaging.kafka.api.OutgoingKafkaRecordMetadata;
 
 @ApplicationScoped
 public class ExpenseSender {
-
-
-//	@Inject
-//	@Channel("expense-out")
-//	Emitter<Expense> sender;
 
 	@Inject
 	@Channel("expense-out-failover")
@@ -31,27 +27,25 @@ public class ExpenseSender {
 	@Channel("expense-out")
 	MutinyEmitter<Expense> mutinyEmitter;
 
+	private static final Logger LOG = Logger.getLogger(ExpenseSender.class);
 
-	//private CompletionStage<Void> sendExpense(Expense expense) {
-		//return sender.send(expense);
-//	}
-
-	private CompletionStage<Void> sendExpenseFailOver(Expense expense){
-		System.out.println("-------------------send expense fail over--------------------------");
-		return failoverSender.send(expense);
+	private void sendExpenseFailOver(Expense expense){
+		failoverSender.send(Message.of(expense)
+				.addMetadata(OutgoingKafkaRecordMetadata.<String> builder()
+						.withHeaders(new RecordHeaders().add("channel", "failover".getBytes(StandardCharsets.UTF_8))).build())
+				);
 	}
 
 
 	private void sendExpenseWithUni(Expense expense){
 		mutinyEmitter.send(expense)
-				.map(x -> expense)
-				.subscribe().with(
-						result -> System.out.println("----------------Message Sent Mutiny-------------"),
-						failure -> {
-							System.out.println("----------------Message NOT Sent Mutiny-------------");
-							expense.desc="Mutiny Failover";
-							sendExpenseFailOver(expense);}
-						);
+		.map(x -> expense)
+		.subscribe().with(
+				result -> LOG.info("------------------Message Sent using the main channel--------------------------"),
+				failure -> {
+					LOG.info("-------------------------------Sending Message using the failover channel---------------------");
+					sendExpenseFailOver(expense);}
+				);
 
 	}
 
